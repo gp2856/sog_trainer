07b8ffb22b650b6a25535332d8ef02dd26510838
GP <gp2856@users.no-reply.github.com>
2018-03-13 21:31:44 -0500

    ^

--- sog_trainer/SoG.h
+++ sog_trainer/SoG.h
@@ -4,7 +4,6 @@
 namespace sog
 {
 	typedef void(__fastcall* tTakeBaseDamage)(void *, int);
-	/*((decltype(tTakeBaseDamage))sog::oTakeBaseDamageAddress)(pThis, 999);*/
 	static tTakeBaseDamage oTakeBaseDamage;
 	static uint32_t addrTakeBaseDamage;
 	
